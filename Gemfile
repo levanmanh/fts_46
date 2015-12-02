7a5522cdffb4e16a9ceac59a44f6ba3423e6ddb2
nguyen anh tuan <nguyen.anh.tuanb@framgia.com>
2015-12-02 13:57:26 +0700

    46983-user-create-exam

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
-
+gem "faker"
 gem "devise"
 gem "config"
 gem "kaminari"
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :users do
     resources :questions
   end
+  resources :exams, only: [:create, :index]
 end
