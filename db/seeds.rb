8cb2c6c66fb6224399ea14e1a182f1c8f5f4d5fd
nguyen anh tuan <nguyen.anh.tuanb@framgia.com>
2015-12-02 08:44:53 +0700

    46979-seed-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Subject.create!(name:  "PHP and MySQL")
+Subject.create!(name:  "Git")
+Subject.create!(name:  "Ruby On Rails")
+Subject.create!(name:  "Android")
+Subject.create!(name:  "Java")
+Subject.create!(name:  "C#")
+User.create!(email: "admin@gmail.com",
+              encrypted_password: "admin1234",
+              password: "admin1234")
