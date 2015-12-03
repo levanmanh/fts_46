2c2474874c6a9359933f7aaefe4c593d47213e6a
nguyen anh tuan <nguyen.anh.tuanb@framgia.com>
2015-12-03 09:49:45 +0700

    46984-user-start-exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,7 @@
 class Exam < ActiveRecord::Base
+  TIME_PER_EXAM = 1200
+  QUESTION_PER_EXAM = 10
+
   enum status: [:opened, :testing, :saving, :checking, :checked]
 
   has_many :sections
@@ -8,6 +11,13 @@ class Exam < ActiveRecord::Base
 
   before_create :create_section
 
+  def time_per_exam
+    Exam::TIME_PER_EXAM / 60
+  end
+
+  def spent_time
+    (Exam::TIME_PER_EXAM - self.duration)/60
+  end
   private
 
   def create_section
